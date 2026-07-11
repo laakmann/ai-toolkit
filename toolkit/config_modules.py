2317bde7cda3264e79a813e751b04fe39e5dce22
Peter Laakmann <laakmann@users.noreply.github.com>
2026-07-11 13:41:59 -0700

    feat(training): add bad-state guard auxiliary loss

--- toolkit/config_modules.py
+++ toolkit/config_modules.py
@@ -350,6 +350,20 @@ class DecoratorConfig:
         self.num_tokens: str = kwargs.get('num_tokens', 4)
 
 
+class BadStateGuardConfig:
+    def __init__(self, **kwargs):
+        self.enabled = kwargs.get("enabled", False)
+        self.path = kwargs.get("path", None)
+        self.multiplier = kwargs.get("multiplier", 0.002)
+        self.probability = kwargs.get("probability", 0.20)
+        self.apply_to_reg = kwargs.get("apply_to_reg", False)
+        self.match_batch_resolution = kwargs.get("match_batch_resolution", True)
+        self.cache_latents = kwargs.get("cache_latents", True)
+        self.match_strategy = kwargs.get("match_strategy", "nearest_aspect")  # nearest_aspect | random
+        self.resize_mode = kwargs.get("resize_mode", "contain")  # contain | cover | stretch
+        self.loss_type = kwargs.get("loss_type", "targeted_flow")
+
+
 ContentOrStyleType = Literal['balanced', 'style', 'content']
 LossTarget = Literal['noise', 'source', 'unaugmented', 'differential_noise']
 
@@ -420,6 +434,7 @@ class TrainConfig:
         self.max_negative_prompts = kwargs.get('max_negative_prompts', 1)
         # multiplier applied to loos on regularization images
         self.reg_weight = kwargs.get('reg_weight', 1.0)
+        self.bad_state_guard = BadStateGuardConfig(**kwargs.get("bad_state_guard", {}))
         self.num_train_timesteps = kwargs.get('num_train_timesteps', 1000)
         # automatically adapte the vae scaling based on the image norm
         self.adaptive_scaling_factor = kwargs.get('adaptive_scaling_factor', False)
